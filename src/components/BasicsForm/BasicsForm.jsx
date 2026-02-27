9d82cf5f63569e174fe804493a37591f5c087499
HugoYah <hugo.hbrc@gmail.com>
2026-02-28 00:25:14 +0100

    feat:Fixing style

--- src/components/BasicsForm/BasicsForm.jsx
+++ src/components/BasicsForm/BasicsForm.jsx
@@ -3,8 +3,8 @@ import './BasicsForm.css'
 import { useState } from 'react';
 
 export default function BasicsForm(){
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
+    
     const [email,setEmail] = useState('')
     const [phone, setPhone] = useState('');
     const [address, setAddress] = useState('');
@@ -13,11 +13,9 @@ export default function BasicsForm(){
     return(
      
         <div className="basics-form">
-            <h1>Fill out information to create your CV !</h1>
+            <h2>Personal Details</h2>
             <div className="fullname-input">
-                <FormInput label="First Name" value={firstName} type="text" id="first-name" required className="input" onChange={e => setFirstName(e.target.value)}></FormInput>
-                <FormInput label="Last Name" value={lastName} type="text" id="last-name" required className="input" onChange={e=>setLastName(e.target.value)}></FormInput>
-            
+                <FormInput label="Full Name" value={fullName} type="text" id="full-name" required className="input" onChange={e => setFullName(e.target.value)}></FormInput>
                 <FormInput label="Email" value={email} type="email" id="email" required className="input" onChange={e=>setEmail(e.target.value)}></FormInput>
                 <FormInput label="Phone" value={phone} type="tel" id="phone" required className="input" onChange={e=>setPhone(e.target.value)}></FormInput>
                 <FormInput label="Address" value={address} type="text" id="address" required className="input" onChange={e=>setAddress(e.target.value)}></FormInput>
